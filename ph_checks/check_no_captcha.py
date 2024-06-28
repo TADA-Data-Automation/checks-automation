0aa4f9e2a1cd76041941908e4819af7aa6f15746
kangweiong <kangggwei@gmail.com>
2024-06-28 13:34:53 +0100

    update headless=new

--- ph_checks/check_no_captcha.py
+++ ph_checks/check_no_captcha.py
@@ -18,7 +18,7 @@ def main(df, partition):
   df = df.copy()
 
   options = webdriver.ChromeOptions()
-  # options.add_argument("--headless")
+  options.add_argument("--headless=new")
   options.add_argument("--no-sandbox")
   options.add_argument("--disable-dev-shm-usage")
   options.add_argument("--disable-gpu")
