1baab0cd47532186d502c66f881a0a1becfb82f6
kangweiong <kangggwei@gmail.com>
2024-06-28 13:18:02 +0100

    change webdriver options

--- ph_checks/check_no_captcha.py
+++ ph_checks/check_no_captcha.py
@@ -18,11 +18,10 @@ def main(df, partition):
   df = df.copy()
 
   options = webdriver.ChromeOptions()
-  options.add_argument("--headless")
+  # options.add_argument("--headless")
   options.add_argument("--no-sandbox")
   options.add_argument("--disable-dev-shm-usage")
   options.add_argument("--disable-gpu")
-  options.add_argument("--window-size=1920x1080")
   driver = webdriver.Chrome(options=options)
   driver.get(os.getenv('PH_URL'))
 
