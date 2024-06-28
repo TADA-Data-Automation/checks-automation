a372633972516ef7ddbccfb0547ec25391b8c5d0
kangweiong <kangggwei@gmail.com>
2024-06-28 12:44:05 +0100

    update window size

--- ph_checks/check_no_captcha.py
+++ ph_checks/check_no_captcha.py
@@ -21,7 +21,7 @@ def main(df, partition):
   options.add_argument("--headless")
   options.add_argument("--no-sandbox")
   options.add_argument("--disable-dev-shm-usage")
-  options.add_argument("--window-size=1920,1200")
+  options.add_argument("--window-size=1920x1200")
   driver = webdriver.Chrome(options=options)
   driver.get(os.getenv('PH_URL'))
 
